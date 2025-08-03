84e16b92f18c0490937d5e25fe55b19b9124ae8b
jai099 <131276939+jai099@users.noreply.github.com>
2025-08-03 20:58:11 +0530

    components created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './index.css'; // Ensure this is imported
+import Navbar from './components/Navbar'; // Ensure this is imported
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">
-        ✅ This is a test..
-      </h1>
-    </div>
+   <Navbar />
   );
 }
 
