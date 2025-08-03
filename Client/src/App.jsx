ec782090fb1dde2a614c158f5b1864b992a231eb
jai099 <131276939+jai099@users.noreply.github.com>
2025-08-03 20:45:07 +0530

    new commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-500">
       <h1 className="text-4xl font-bold text-white">
-        ✅ Tailwind CSS is Working!
+        ✅ This is a test
       </h1>
     </div>
   );
